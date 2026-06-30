4e0f295faad2922de77f7f964bbf203feb1eaa32
srinivasmerugu99 <sriniivasmerugu@gmail.com>
2026-06-30 11:14:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,18 +18,18 @@ from logging.handlers import RotatingFileHandler
 #rohit_1888 on Tg
 #--------------------------------------------
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8154426339:")
-APP_ID = int(os.environ.get("APP_ID", "")) #Your API ID from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "") #Your API Hash from my.telegram.org
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8810354889:AAGPNMmHDJBDYsHHBRz57_TJ5NlQnWDySwc")
+APP_ID = int(os.environ.get("APP_ID", "31235022")) #Your API ID from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "64a8cdb2c92d7ac433765af3e65dabb1") #Your API Hash from my.telegram.org
 #--------------------------------------------
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002170811388")) #Your db channel Id
-OWNER = os.environ.get("OWNER", "sewxiy") # Owner username without @
-OWNER_ID = int(os.environ.get("OWNER_ID", "7328629001")) # Owner id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1004322372866")) #Your db channel Id
+OWNER = os.environ.get("OWNER", "Baddakam_Bob") # Owner username without @
+OWNER_ID = int(os.environ.get("OWNER_ID", "8202727275")) # Owner id
 #--------------------------------------------
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Vasu:@Daemon990@cluster0.rv1vrmg.mongodb.net/?appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluooo")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
